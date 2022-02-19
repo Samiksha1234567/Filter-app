81dc75750bf5c7d5fe492c7d5fde12bdc460aedc
Samiksha1234567 <88656460+Samiksha1234567@users.noreply.github.com>
2022-02-19 15:01:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function preload(){
+
+}
+function setup(){
+  canvas= createCanvas(900,700);
+  canvas.center();  
+}
+function draw(){
+
+}
+function take_snapshot(){
+    save('myFilterImage.png');
+}
